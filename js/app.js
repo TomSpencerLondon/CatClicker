568544454357be80db8fcf1129ebd0dcb63a600c
tomspencerlondon <tomspencer_uk@yahoo.co.uk>
2018-03-10 19:34:22 +0000

    Add event listener for second image

--- js/app.js
+++ js/app.js
@@ -1,16 +1,27 @@
-let clicks = 0;
-
+let clicks1 = 0;
+let clicks2 = 0; 
 
 function addClickListener(){
-  $('.image').click(function(event) {       
-          addClicks();
+  $('.image1').click(function(event) {       
+          addClicks1();
           event.preventDefault(); 
+  });
+
+  $('.image2').click(function(event) {       
+    addClicks2();
+    event.preventDefault(); 
   });
 }
 
-function addClicks(){
-  clicks += 1;
-  $(".clicks").html(clicks)
+
+function addClicks1(){
+  clicks1 += 1;
+  $(".clicks1").html(clicks1)
+}
+
+function addClicks2(){
+  clicks2 += 1;
+  $(".clicks2").html(clicks2)
 }
 
 $(window).ready(function(){
